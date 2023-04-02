47a129edbcf7a96f6ccc118c5ee6ae69c39b271a
Anna Maras <aniamaras@wp.pl>
2023-04-02 18:09:03 +0200

    task2 modified

--- app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
+++ app/src/main/java/pl/futurecollars/invoicing/db/memory/InMemoryDatabase.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import java.util.Optional;
 import pl.futurecollars.invoicing.db.Database;
 import pl.futurecollars.invoicing.model.Invoice;
+
 public class InMemoryDatabase implements Database {
   private final Map<Integer, Invoice> invoices = new HashMap<>();
   private int nextId = 1;
