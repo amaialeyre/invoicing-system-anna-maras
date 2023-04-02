e355256d6337335f86956fd05499726f30b4e192
Anna Maras <aniamaras@wp.pl>
2023-04-02 19:42:14 +0200

    task2

--- app/src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/InvoiceEntry.java
@@ -8,12 +8,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class InvoiceEntry {
+
   private String description;
   private BigDecimal price;
   private BigDecimal vatValue;
   private Vat vatRate;
 
   public InvoiceEntry(String description, BigDecimal price, BigDecimal vatValue, Vat vatRate) {
+
     this.description = description;
     this.price = price;
     this.vatValue = vatValue;
