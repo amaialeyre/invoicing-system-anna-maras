ba83453fc860458260b3c21c8bd01d43cd06e818
Anna Maras <aniamaras@wp.pl>
2023-04-02 19:53:20 +0200

    task2

--- app/src/main/java/pl/futurecollars/invoicing/model/Company.java
+++ app/src/main/java/pl/futurecollars/invoicing/model/Company.java
@@ -7,11 +7,13 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 
 public class Company {
+
   private String taxIdentificationNumber;
   private String address;
   private String name;
 
   public Company(String taxIdentificationNumber, String address, String name) {
+
     this.taxIdentificationNumber = taxIdentificationNumber;
     this.address = address;
     this.name = name;
